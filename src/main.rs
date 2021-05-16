dcc6c18b3649e0393d89a69a4dbb21395cbda23e
VaranTavers <tasnadi98@gmail.com>
2021-05-16 11:12:30 +0300

    WC works theoretically

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::collections::HashMap;
 
+use std::io::BufWriter;
+use std::io::Write;
+
+use std::fs::File;
+
 mod word_count;
 
 fn wc_all_files(folder_name: &str) -> std::io::Result<HashMap<String, i32>> {
@@ -15,7 +20,29 @@ fn wc_all_files(folder_name: &str) -> std::io::Result<HashMap<String, i32>> {
     Ok(dict)
 }
 
+fn cull_word_map(dict: HashMap<String, i32>, high: usize, low: usize) -> Vec<String> {
+    let mut counts = dict.values().cloned().collect::<Vec<i32>>();
+    counts.sort_unstable();
+    counts.reverse();
+    let high_threshold = counts[high];
+    let low_threshold = counts[low];
+
+    let words = dict.keys()
+                    .filter(|k| dict[*k] > low_threshold && dict[*k] < high_threshold)
+                    .cloned()
+                    .collect::<Vec<String>>();
+
+    words
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    let hmap = wc_all_files("./korpusz").unwrap();
+    let words = cull_word_map(hmap, 99, 10099);
+
+    let out = File::create("word_index.csv").unwrap();
+    let mut writer = BufWriter::new(out);
+    for (w, i) in words.iter().enumerate() {
+        writer.write_all(format!("{}; {}\n", w, i).as_bytes()).unwrap();
+    }
 }
